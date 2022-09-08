dbb372fae8a56cfd83514ab16ee5df01020ae4e4
chauanphu <chauanphu@gmail.com>
2022-09-08 16:32:41 +0700

    add build

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const path = require('path')
 var cors = require('cors')
+require('dotenv').config()
 
 //const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "./client")))
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://bhld-95d22.as.r.appspot.com/");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // middleware
 app.use(express.json());
@@ -13,7 +23,10 @@ app.use(cors())
 //routes
 app.use("/api", routes);
 
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'))
+})
 //set up
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("Listening on port " + process.env.PORT);
 });
