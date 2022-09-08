14507de8d24f7ba2c8d1296fea15d7ac6840be4d
chauanphu <chauanphu@gmail.com>
2022-09-08 19:55:10 +0700

    remove frontend

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ const path = require('path')
 var cors = require('cors')
 require('dotenv').config()
 
-//const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, "./build")))
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://bhld-95d22.as.r.appspot.com/");
@@ -22,9 +19,6 @@ app.use(cors())
 //routes
 app.use("/api", routes);
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
 //set up
 app.listen(process.env.PORT || 8080, () => {
   console.log("Listening on port " + process.env.PORT);
