2d288c451b424377724fc59ac51e0d9e8e4f01c6
chauanphu <chauanphu@gmail.com>
2022-10-03 11:06:12 +0700

    CRUD categories

--- services/category_service.js
+++ services/category_service.js
@@ -3,10 +3,9 @@
 const CategoryCollection = 'categories'
 const Command = require('./mongodb')
 var command = new Command(CategoryCollection)
+const { ObjectId } = require('mongodb');
 
 const Category = {
-    _value_: [],
-
     async get_all() {
         const _array_ = await command.init()
             .then(col => {
@@ -17,6 +16,37 @@ const Category = {
             length: _array_.length,
             data: [..._array_]
         }
+    },
+    async get_one(id) {
+        const _result_ = await command.init()
+            .then(col => {
+                return col.findOne({ "_id": ObjectId(id) })
+            })
+        command.close()
+        return _result_
+    },
+    async add(data) {
+        await command.init()
+            .then(col => {
+                return col.insertOne(data)
+            })
+        command.close()
+    },
+    async update(id, data) {
+        const _result_ = await command.init()
+            .then(col => {
+                return col.updateOne({ "_id": ObjectId(id) }, { $set: data });
+            })
+        command.close()
+        return _result_
+    },
+    async delete(id) {
+        const _result_ = await command.init()
+            .then(col => {
+                return col.deleteOne({ "_id": ObjectId(id) })
+            })
+        command.close()
+        return _result_
     }
 }
 module.exports = Category
