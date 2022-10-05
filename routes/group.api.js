38aa4295be144ee968b3a8f2d43a6a6763f8c0a5
chauanphu <chauanphu@gmail.com>
2022-10-05 18:18:14 +0700

    disable group crud api

--- routes/group.api.js
+++ routes/group.api.js
@@ -1,37 +1,38 @@
-var router = require('express').Router();
+// var router = require('express').Router();
 
-var Group = require('../services/group.service')
+// var Group = require('../services/group.service')
 
-// api/categories
-router.get('/', async function (req, res) {
-    var { length, data } = await Group.get_all()
-    res.setHeader('Content-Range', length)
-    res.json(data);
-});
+// // api/categories
+// router.get('/', async function (req, res) {
+//     var { length, data } = await Group.get_all()
+//     res.setHeader('Content-Range', length)
+//     res.json(data);
+// });
 
-// api/products/:id
-router.get('/:id', async function (req, res) {
-    const id = req.params.id
-    res.json(await Group.get_one(id));
-});
+// // api/products/:id
+// router.get('/:id', async function (req, res) {
+//     const id = req.params.id
+//     res.json(await Group.get_one(id));
+// });
 
-router.post('/', async function (req, res) {
-    const _result_ = await Group.add(req.body)
-    if (_result_.modifiedCount == 0) res.status(400).end()
-    res.status(200).end()
-});
+// router.post('/', async function (req, res) {
+//     const _result_ = await Group.add(req.body)
+//     console.log(_result_)
+//     // if (_result_.modifiedCount == 0) res.status(400).end()
+//     res.status(200).end()
+// });
 
-router.put('/:id', async function (req, res) {
-    const id = req.params.id
-    const _result_ = await Group.update(id, req.body)
-    if (_result_.modifiedCount == 0) res.status(400).end()
-    else res.status(200).end()
-});
+// router.put('/:id', async function (req, res) {
+//     const id = req.params.id
+//     const _result_ = await Group.update(id, req.body)
+//     if (_result_.modifiedCount == 0) res.status(400).end()
+//     else res.status(200).end()
+// });
 
-router.delete('/:id', async function (req, res) {
-    const id = req.params.id
-    const _result_ = await Group.delete(id)
-    res.status(200).end()
-});
+// router.delete('/:id', async function (req, res) {
+//     const id = req.params.id
+//     const _result_ = await Group.delete(id)
+//     res.status(200).end()
+// });
 
-module.exports = router;
+// module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var router = require('express').Router();
 var product = require('./product.api')
 var category = require('./category.api')
-var group = require('./group.api')
+// var group = require('./group.api')
 
 // split up route handling
 router.use('/products', product);
 router.use('/categories', category);
-router.use('/groups', group);
+// router.use('/groups', group);
 // etc.
 
 module.exports = router;
